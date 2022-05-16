f46b08b9e12f04be4824bd2ba22cc11d07e0e7f5
Javier <35864946+jmagit@users.noreply.github.com>
2022-05-16 17:24:36 +0200

    entorno de configuración

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,18 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{process.env.REACT_APP_NOMBRE}</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="btn btn-info"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <h1>Secreto: {process.env.REACT_APP_SECRET}</h1>
       </header>
     </div>
   );
