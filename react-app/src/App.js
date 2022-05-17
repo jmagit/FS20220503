ce58ed9d684ac834761970237fe9b62cd2ecbd5e
Javier <35864946+jmagit@users.noreply.github.com>
2022-05-17 10:21:02 +0200

    JSX

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const elelement = (
+    <>
+      <h1>Hola mundo</h1>
+      <h2>Que tal</h2>
+    </>
+  )
+  let nombre = 'Valladolid'
+  let listado = [
+    { "id": 1, "nombre": "Camelo", "apellidos": "Coton", "edad": 37, "telefonos": ["12342", "3453", "3334"] },
+    { "id": 2, "nombre": "Pepito", "apellidos": "Grillo", "edad": 67 },
+    { "id": 3, "nombre": "Pierre", "apellidos": "Nodoiuna", "edad": 55 },
+    { "id": 4, "nombre": "Capitan", "apellidos": "Tan", "edad": 18 },
+  ]
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +33,13 @@ function App() {
         </a>
         <h1>Secreto: {process.env.REACT_APP_SECRET}</h1>
       </header>
+      {elelement}
+      <h3 className='alert alert-danger'>Hola {nombre.toUpperCase()}!!!</h3>
+      <ul>
+        {listado.map(item => (
+          <li key={item.id}>{item.nombre} {item.apellidos}</li>
+        ))}
+      </ul>
     </div>
   );
 }
