939c98b4a7237f32ca3d56f8abdd1924e30be265
Javier <jmartin@grupoloyal.com>
2022-05-19 16:27:11 +0200

    validaciones

--- react-app/src/formulario.js
+++ react-app/src/formulario.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ValidationMessage } from './comunes';
-
+import { esNIF } from './biblioteca'
 export default class Formulario extends React.Component {
     constructor(props) {
         super(props)
@@ -13,7 +13,7 @@ export default class Formulario extends React.Component {
                 recontraseña: '',
                 nif: '',
             },
-            esInvalido: true,
+            esInvalido: false,
             errores: {}
         }
         this.form = React.createRef();
@@ -28,31 +28,62 @@ export default class Formulario extends React.Component {
             elemento[cmp] = value
             return { elemento }
         });
+        this.validate()
+    }
 
+    componentDidMount() {
+        this.validate()
+    }
+    validate() {
+        if (this.form) {
+            let esInvalido = false
+            let errores = {}
+            for (let cntr of this.form.current.elements) {
+                if (cntr.name) {
+                    // eslint-disable-next-line default-case
+                    switch (cntr.name) {
+                        case 'nif':
+                            if (cntr.value && !esNIF(cntr.value))
+                                cntr.setCustomValidity('No es un NIF valido')
+                            else
+                                cntr.setCustomValidity('')
+                            break;
+
+                    }
+                    errores[cntr.name] = cntr.validationMessage;
+                    if(!cntr.checkValidity()) esInvalido = true
+                }
+            }
+            this.setState({ errores, esInvalido });
+        }
     }
 
     render() {
-        return (
+         return (
             <form ref={this.form}>
                 <p>
                     <label htmlFor='id'>código:</label>
-                    <input type='number' id='id' name='id' value={this.state.elemento.id} onChange={this.handleChange} />
+                    <input type='number' id='id' name='id' value={this.state.elemento.id} onChange={this.handleChange}
+                        required />
                     <ValidationMessage msg={this.state.errores.id} />
                     <br />
                     <label htmlFor='email'>correo:</label>
-                    <input type='email' id='email' name='email' value={this.state.elemento.email} onChange={this.handleChange} />
+                    <input type='email' id='email' name='email' value={this.state.elemento.email} onChange={this.handleChange}
+                        required minLength={3} />
                     <ValidationMessage msg={this.state.errores.email} />
                     <br />
                     <label htmlFor='nombre'>nombre:</label>
-                    <input type='text' id='nombre' name='nombre' value={this.state.elemento.nombre} onChange={this.handleChange} />
+                    <input type='text' id='nombre' name='nombre' value={this.state.elemento.nombre} onChange={this.handleChange} minLength={2} />
                     <ValidationMessage msg={this.state.errores.nombre} />
                     <br />
                     <label htmlFor='contraseña'>contraseña:</label>
-                    <input type='password' id='contraseña' name='contraseña' value={this.state.elemento.contraseña} onChange={this.handleChange} />
+                    <input type='password' id='contraseña' name='contraseña' value={this.state.elemento.contraseña} onChange={this.handleChange}
+                        required />
                     <ValidationMessage msg={this.state.errores.contraseña} />
                     <br />
                     <label htmlFor='recontraseña'>repite contraseña:</label>
-                    <input type='password' id='recontraseña' name='recontraseña' value={this.state.elemento.recontraseña} onChange={this.handleChange} />
+                    <input type='password' id='recontraseña' name='recontraseña' value={this.state.elemento.recontraseña} onChange={this.handleChange}
+                        required  />
                     <ValidationMessage msg={this.state.errores.recontraseña} />
                     <br />
                     <label htmlFor='nif'>nif:</label>
@@ -60,8 +91,18 @@ export default class Formulario extends React.Component {
                     <ValidationMessage msg={this.state.errores.nif} />
                 </p>
                 <p>
-                    <input type="button" value="enviar" disabled={this.state.esInvalido} />
-                    <input type="button" value="volver"  />
+                    <input type="button" value="enviar" disabled={this.state.esInvalido} onClick={() => { 
+                        let elemento = this.state.elemento
+                        delete elemento.recontraseña
+                        alert(JSON.stringify(elemento))
+                        }} />
+                    <input type="button" value="volver" />
+       <input type="button" value="cargar" onClick={() => this.setState({elemento: {id: 99,
+            email: 'p@g',
+            nombre: 'Pepito',
+            contraseña: 'kkkk',
+            recontraseña: 'kkkk',
+            nif: '4g',}})} />
                 </p>
             </form>
         )
