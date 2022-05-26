ab58d3bdfa0001c3550fbb6cb62e47e4fb262626
Javier <35864946+jmagit@users.noreply.github.com>
2022-05-26 16:10:57 +0200

    APIs

--- movil-app/components/calculadora.js
+++ movil-app/components/calculadora.js
@@ -1,9 +1,293 @@
-import { Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 
-export default function Calculadora() {
+const Pantalla = props => (
+    <Text style={props.style}>
+        {props.coma ? props.pantalla.replace(/\./g, ",") : props.pantalla}
+    </Text>
+)
+
+const Resumen = ({ coma, resumen, style }) => (
+    <Text style={style}>
+        {coma ? resumen.replace(/\./g, ",") : resumen}
+    </Text>
+)
+
+const BtnCalcular = ({ texto, onClick, style }) => {
+    const handleClick = () => {
+        if (onClick) onClick(texto);
+    };
+    return (
+        // <Button style={style} onPress={handleClick} title={texto} />
+        <TouchableOpacity style={style} onPress={handleClick} >
+            <Text style={{ fontSize: 28, color: '#b5b7bb' }}>{texto}</Text>
+        </TouchableOpacity>
+    );
+}
+
+export default function Calculadora(props) {
+    const [darkMode, setDarkMode] = useState(false);
+    const [pantalla, setPantalla] = useState(props.init ? props.init.toString() : '0');
+    const [resumen, setResumen] = useState('');
+    const [acumulado, setAcumulado] = useState(0);
+    const [operador, setOperador] = useState('+');
+    const [limpiar, setLimpiar] = useState(true);
+
+    const inicia = () => {
+        setAcumulado(0)
+        setOperador('+')
+        setLimpiar(true)
+        setPantalla('0')
+        setResumen('')
+    }
+    const ponDigito = (value) => {
+        if (typeof value !== "string") {
+            value = value.toString();
+        }
+        if (value.length !== 1 || value < "0" || value > "9") {
+            console.error("No es un valor numérico.");
+            return;
+        }
+        if (limpiar || pantalla === "0") {
+            setPantalla(value)
+            setLimpiar(false)
+        } else {
+            setPantalla(pantalla + value)
+        }
+    }
+    const ponOperando = (value) => {
+        // eslint-disable-next-line eqeqeq
+        if (!Number.isNaN(parseFloat(value)) && parseFloat(value) == value) {
+            setPantalla(value);
+            setLimpiar(false);
+        } else {
+            console.error('No es un valor numérico.');
+        }
+    }
+
+    const ponComa = () => {
+        if (limpiar) {
+            if (!isFinite(acumulado) || isNaN(acumulado)) {
+                return;
+            }
+            setPantalla("0.");
+            setLimpiar(false);
+        } else if (pantalla.indexOf(".") === -1) {
+            setPantalla(pantalla + ".");
+        } else
+            //store.AddNotify('Ya está la coma');
+            console.warn('Ya está la coma');
+    }
+    const borrar = () => {
+        if (limpiar || pantalla.length === 1) {
+            setPantalla("0");
+            setLimpiar(true);
+        } else {
+            setPantalla(pantalla.substr(0, pantalla.length - 1));
+        }
+    }
+    const cambiaSigno = () => {
+        setPantalla((-pantalla).toString());
+        if (limpiar) {
+            setAcumulado(-acumulado);
+        }
+    }
+    const calcula = (value) => {
+        if ("+-*/=".indexOf(value) === -1) {
+            console.error(`Operación no soportada: ${value}`);
+            return;
+        }
+        const operando = parseFloat(pantalla);
+        let resultado = acumulado
+        switch (operador) {
+            case "+":
+                resultado += operando;
+                break;
+            case "-":
+                resultado -= operando;
+                break;
+            case "*":
+                resultado *= operando;
+                break;
+            case "/":
+                resultado /= operando;
+                break;
+            case "=":
+            default:
+                break;
+        }
+        setAcumulado(resultado)
+        // Con eval()
+        // setAcumulado(val (acumulado + operador + pantalla));
+        setResumen(value === '=' ? '' : (`${resultado} ${value}`));
+        // Number: double-precision IEEE 754 floating point.
+        // 9.9 + 1.3, 0.1 + 0.2, 1.0 - 0.9
+        setPantalla(parseFloat(resultado.toPrecision(15)).toString());
+        setOperador(value);
+        setLimpiar(true);
+        if (props.onChange) props.onChange(resultado);
+    }
+
+    //const styles = StyleSheet.compose(stylesCommons, darkMode ? darkTheme: lightTheme)
+    const styles = StyleSheet.create({
+        visor: {
+            backgroundColor: darkMode ? '#282f3b' : '#f5f5f5',
+            maxWidth: '100%',
+            minHeight: '35%',
+            alignItems: 'flex-end',
+            justifyContent: 'flex-end',
+        },
+        pantalla: {
+            maxHeight: 45,
+            color: '#00b9d6',
+            margin: 15,
+            fontSize: 35,
+        },
+        resumen: {
+            color: darkMode ? '#B5B7BB' : '#7c7c7c',
+            fontSize: 20,
+            marginRight: 10,
+            alignSelf: 'flex-end',
+        },
+        botones: {
+            width: '100%',
+            height: '40%',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+        },
+        boton: {
+            borderColor: darkMode ? '#3f4d5b' : '#e5e5e5',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minWidth: '24%',
+            minHeight: '54%',
+            flex: 2,
+        },
+        textoBoton: {
+            color: darkMode ? '#b5b7bb' : '#7c7c7c',
+            fontSize: 28,
+        },
+        btnOperar: {
+            color: 'white',
+            backgroundColor: darkMode ? '#414853' : '#00b9d6'
+        },
+        btnComando: {
+            backgroundColor: darkMode ? '#414853' : '#ededed'
+        },
+        btnDigito: {
+            backgroundColor: darkMode ? '#303946' : '#fff'
+        }
+    })
+
     return (
         <View>
-            <Text>Esto es la calculadora</Text>
+            <View style={styles.visor}>
+                <Text style={styles.resumen}>{resumen}</Text>
+                <Text style={styles.pantalla}>{pantalla}</Text>
+            </View>
+            <View style={styles.botones}>
+                <BtnCalcular style={[styles.boton, styles.btnComando]} texto="CE" onClick={inicia} />
+                <BtnCalcular style={[styles.boton, styles.btnComando, { minWidth: '49%' }]} texto={'\u21B6 BORRAR'} onClick={borrar} />
+                <BtnCalcular style={[styles.boton, styles.btnOperar]} texto="+" onClick={calcula} />
+                {[7, 8, 9].map(item => (
+                    <BtnCalcular key={"btn" + item} style={[styles.boton, styles.btnDigito]} texto={item} onClick={ponDigito} />
+                ))}
+                <BtnCalcular style={[styles.boton, styles.btnOperar]} texto="-" onClick={calcula} />
+                {[4, 5, 6].map(item => (
+                    <BtnCalcular key={"btn" + item} style={[styles.boton, styles.btnDigito]} texto={item} onClick={ponDigito} />
+                ))}
+                <BtnCalcular style={[styles.boton, styles.btnOperar]} texto="*" onClick={calcula} />
+                {[1, 2, 3].map(item => (
+                    <BtnCalcular key={"btn" + item} style={[styles.boton, styles.btnDigito]} texto={item} onClick={ponDigito} />
+                ))}
+                <BtnCalcular style={[styles.boton, styles.btnOperar]} texto="/" onClick={calcula} />
+                <BtnCalcular style={[styles.boton, styles.btnDigito]} texto="±" onClick={cambiaSigno} />
+                <BtnCalcular style={[styles.boton, styles.btnDigito]} texto="0" onClick={ponDigito} />
+                <BtnCalcular style={[styles.boton, styles.btnDigito]} texto={props.coma ? "," : "."} onClick={ponComa} />
+                <BtnCalcular style={[styles.boton, styles.btnOperar]} texto="=" onClick={calcula} />
+            </View>
         </View>
     )
 }
+
+const stylesCommons = StyleSheet.create({
+    visor: {
+        maxWidth: '100%',
+        minHeight: '35%',
+        alignItems: 'flex-end',
+        justifyContent: 'flex-end',
+    },
+    pantalla: {
+        maxHeight: 45,
+        color: '#00b9d6',
+        margin: 15,
+        fontSize: 35,
+    },
+    resumen: {
+        fontSize: 20,
+        marginRight: 10,
+        alignSelf: 'flex-end',
+    },
+    botones: {
+        width: '100%',
+        height: '35%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    boton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        minWidth: '24%',
+        minHeight: '54%',
+        flex: 2,
+    },
+    textoBoton: {
+        fontSize: 28,
+    },
+    btnOperar: {
+    },
+    btnDigito: {
+    }
+})
+const darkTheme = StyleSheet.create({
+    visor: {
+        backgroundColor: '#282f3b',
+    },
+    resumen: {
+        color: '#B5B7BB',
+    },
+    boton: {
+        borderColor: '#3f4d5b',
+    },
+    textoBoton: {
+        color: '#b5b7bb',
+    },
+    btnOperar: {
+        backgroundColor: '#414853'
+    },
+    btnDigito: {
+        backgroundColor: '#303946'
+    }
+})
+const lightTheme = StyleSheet.create({
+    visor: {
+        backgroundColor: '#f5f5f5',
+    },
+    resumen: {
+        color: '#7c7c7c',
+    },
+    boton: {
+        borderColor: '#e5e5e5',
+    },
+    textoBoton: {
+        color: '#7c7c7c',
+    },
+    btnOperar: {
+        backgroundColor: '#ededed'
+    },
+    btnDigito: {
+        backgroundColor: '#fff'
+    }
+})
+
+
--- movil-app/components/demos.js
+++ movil-app/components/demos.js
@@ -1,16 +1,176 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, ScrollView, FlatList, SectionList, Image } from 'react-native';
+import {
+    StyleSheet, Text, View, Button, ScrollView, FlatList, SectionList, Image, ActivityIndicator,
+    TextInput, KeyboardAvoidingView, Platform, Keyboard, Switch, Modal, Share, Vibration,
+    TouchableHighlight, TouchableNativeFeedback, TouchableOpacity, TouchableWithoutFeedback,
+} from 'react-native';
 import Contador from './contador';
 import importImg from '../assets/favicon.png'
 import data from '../data/contactos.json'
 import Calculadora from './calculadora';
+import styles from '../theme/styles';
 
 export default function Demos() {
     console.log('Iniciando aplicación')
     return (
-        <View>
-            <Calculadora />
+        <View style={{ flex: 1 }}>
+            <ShareExample style={[styles.texto, styles.error, { color: 'yellow'}]} />
             <StatusBar hidden />
+        </View>
+    )
+}
+
+const ShareExample = () => {
+    const onShare = async () => {
+      try {
+        const result = await Share.share({
+          message:
+            'React Native | A framework for building native apps using React',
+        });
+        if (result.action === Share.sharedAction) {
+          if (result.activityType) {
+            // shared with activity type of result.activityType
+          } else {
+            // shared
+          }
+        } else if (result.action === Share.dismissedAction) {
+          // dismissed
+        }
+      } catch (error) {
+        alert(error.message);
+      }
+    };
+    const SECONDS = 1000;
+
+    return (
+      <View style={{ marginTop: 50 }}>
+        <Button onPress={onShare} title="Share" />
+        <Button onPress={() => Vibration.vibrate([1 * SECONDS, 5 * SECONDS, 2 * SECONDS], true)} title="vibratem" />
+      </View>
+    );
+  };
+
+const EstilosView = ({style}) => {
+    console.log(style)
+    console.log(Object.assign({}, ...style))
+    return (
+        <View style={styles.containerFluid}>
+            <Text style={styles.texto}>Uno 
+            <Text style={{fontSize: 12}}>Pequeño</Text>
+            <Text style={styles.cursiva}>anidado</Text>
+            </Text>
+            <TouchableWithoutFeedback onPress={()=>alert('click')}>
+                <Text style={[styles.texto, styles.error, { color: 'yellow'}]}>Dos</Text>
+            </TouchableWithoutFeedback>
+            <Text>Tres</Text>
+            <Text style={{color: styles.containerFluid.color}}>Cuatro</Text>
+            <Text>Cinco</Text>
+            <Button title='Boton' style={{backgroundColor: 'red', color: 'yellow'}} color='red' onPress={()=>alert('click')} />
+
+        </View>
+        
+    )
+}
+
+const ModalView = ({ texto }) => {
+    const [modalVisible, setModalVisible] = React.useState(false)
+    const [nombre, setNombre] = React.useState('mundo')
+    const [isEnabled, setIsEnabled] = React.useState(false)
+    const modalStyles = StyleSheet.create({
+        centeredView: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginTop: 22,
+        },
+        modalView: {
+            margin: 20,
+            backgroundColor: 'white',
+            borderRadius: 20,
+            padding: 35,
+            alignItems: 'center',
+            shadowColor: '#000',
+            shadowOffset: {
+                width: 0,
+                height: 2,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 5,
+        },
+        modalText: {
+            marginBottom: 15,
+            textAlign: 'center',
+        },
+    })
+
+    return (
+        <View style={modalStyles.centeredView}>
+            <Modal animationType='slide' transparent visible={modalVisible}>
+                <View style={modalStyles.centeredView}>
+                    <View style={modalStyles.modalView}>
+                        <Text style={modalStyles.modalText}>{texto}</Text>
+                        <Switch
+                            trackColor={{ false: '#767577', true: '#81b0ff' }}
+                            thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                            ios_backgroundColor='#3e3e3e'
+                            value={isEnabled} onValueChange={() => setIsEnabled(previousState => !previousState)}
+                        />
+                        <Button title='Cerrar' onPress={() => setModalVisible(!modalVisible)} />
+                    </View>
+                </View>
+            </Modal>
+            <Text>{isEnabled ? 'Hola' : 'Adios'} {nombre}</Text>
+            <Button title='Ver modal' onPress={() => setModalVisible(true)} />
+        </View>
+    )
+}
+
+const EntradasTextInput = () => {
+    const [text, onChangeText] = React.useState('Hola mundo')
+    const [number, onChangeNumber] = React.useState(null)
+    const styles = StyleSheet.create({
+        input: {
+            borderWidth: 1,
+            color: 'red',
+            fontSize: 24,
+            margin: 5
+        },
+        textoGrande: {
+            marginTop: 50,
+            fontSize: 48
+        }
+
+    })
+    return (
+        <View>
+            <Button title='cierra' onPress={() => Keyboard.dismiss()} />
+            <ToggleButton />
+            <TouchableNativeFeedback onPress={() => Keyboard.dismiss()}>
+                <Text style={styles.textoGrande}>{text}</Text>
+            </TouchableNativeFeedback>
+            <TextInput style={styles.input} multiline numberOfLines={4}
+                value={text} onChangeText={onChangeText}
+            />
+            <Text style={styles.textoGrande}>{number}</Text>
+            <TextInput style={styles.input} value={number} onChangeText={onChangeNumber}
+                placeholder='introduce un número' keyboardType='numeric'
+            />
+        </View>
+    )
+}
+const ToggleButton = () => {
+    const [isEnabled, setIsEnabled] = React.useState(false)
+
+    return (
+        <View>
+            <Switch
+                trackColor={{ false: '#767577', true: '#81b0ff' }}
+                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                ios_backgroundColor='#3e3e3e'
+                value={isEnabled} onValueChange={() => setIsEnabled(previousState => !previousState)}
+            />
         </View>
     )
 }
@@ -78,23 +238,3 @@ function ClasificaLista() {
     )
 
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    item: {
-        backgroundColor: "#f9c2ff",
-        padding: 20,
-        marginVertical: 8
-    },
-    header: {
-        fontSize: 32,
-        backgroundColor: "#fff"
-    },
-    title: {
-        fontSize: 24
-    }
-});
