60858191ff8c5d046bd3ba264f4356a9d4e3499e
Javier <jmartin@grupoloyal.com>
2022-06-01 15:13:37 +0200

    datos

--- backend/mi-servidor/routes/users.js
+++ backend/mi-servidor/routes/users.js
@@ -5,5 +5,18 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.route('/login.jpg')
+    .get(function (req, res) {
+        res.render('login', { user: "", password: "P@$$w0rd", intentos: 3 } )
+    })
+    .post(async function (req, res) {
+        // { "user": "admin", "password": "P@$$w0rd"}
+        if(req.body.user === "admin" && req.body.password === "P@$$w0rd") {
+            if(req.query.continue)
+            res.redirect(req.query.continue)
+            else
+            res.redirect('/')
+        } else 
+          res.render('login', {user: req.body.user, password: req.body.password, intentos: req.body.intentos-1} )
+    })
 module.exports = router;
