231b5cbda8706c4f66e2eca759e70c956b32e0ce
Javier <35864946+jmagit@users.noreply.github.com>
2022-06-09 16:43:31 +0200

    Entidades

--- java/demo/src/main/java/com/example/demo/DemoApplication.java
+++ java/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.domains.contracs.repositories.ActorRepository;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -15,11 +18,16 @@ public class DemoApplication implements CommandLineRunner {
 	@Autowired
 	Persona persona;
 	
+	@Autowired
+	ActorRepository dao;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		persona.saluda();
 		System.out.println("Hola mundo");
 		persona.despide();
+		
+		dao.findAll().forEach(item -> System.out.println(item));
 	}
 
 }
