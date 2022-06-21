e2a342ed496ae8888872561ee71227c8bd0cc67b
Javier <jmartin@grupoloyal.com>
2022-06-21 12:10:19 +0200

    JUnit

---
+++ java/demo/src/main/java/com/example/demo/Calculadora.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Calculadora {
+	public double suma(double a, double b) {
+		return a + b;
+	}
+	public double divide(double a, double b) {
+		if(b == 0)
+			throw new ArithmeticException("/ by 0");
+		return a / b;
+	}
+	public int divide(int a, int b) {
+		return a / b;
+	}
+}
